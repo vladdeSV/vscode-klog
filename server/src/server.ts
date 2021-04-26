604b663356a931b17847e275749bb8adcba9d276
Vladimirs Nordholm <v@vladde.net>
2021-04-26 13:37:55 +0200

    minor cleanup

--- server/src/server.ts
+++ server/src/server.ts
@@ -137,6 +137,10 @@ documents.onDidChangeContent(change => {
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 
+
+    // // In this simple example we get the settings for every validate run.
+    // const settings = await getDocumentSettings(textDocument.uri);
+
     const tmp = require('tmp');
     const util = require('util');
     const exec = util.promisify(require('child_process').exec);
@@ -165,58 +169,26 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
             message: error.title,
             source: 'klog'
         };
-        diagnostics.push(diagnostic);
+
+        // todo why does this check exist?
+        if (hasDiagnosticRelatedInformationCapability) {
+            diagnostic.relatedInformation = [
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range)
+                    },
+                    message: error.details
+                },
+            ];
+        }
+
+        diagnostics.push(diagnostic)
     }
 
     connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 
     tempFile.removeCallback();
-
-
-    // // In this simple example we get the settings for every validate run.
-    // const settings = await getDocumentSettings(textDocument.uri);
-
-    // // The validator creates diagnostics for all uppercase words length 2 and more
-    // const text = textDocument.getText();
-    // const pattern = /\b[A-Z]{2,}\b/g;
-    // let m: RegExpExecArray | null;
-
-    // let problems = 0;
-    // const diagnostics: Diagnostic[] = [];
-    // while ((m = pattern.exec(text)) && problems < (settings?.maxNumberOfProblems ?? 100) /* fixme fix settings */) {
-    //     problems++;
-    //     const diagnostic: Diagnostic = {
-    //         severity: DiagnosticSeverity.Warning,
-    //         range: {
-    //             start: textDocument.positionAt(m.index),
-    //             end: textDocument.positionAt(m.index + m[0].length)
-    //         },
-    //         message: `${m[0]} is all uppercase.`,
-    //         source: 'ex'
-    //     };
-    //     if (hasDiagnosticRelatedInformationCapability) {
-    //         diagnostic.relatedInformation = [
-    //             {
-    //                 location: {
-    //                     uri: textDocument.uri,
-    //                     range: Object.assign({}, diagnostic.range)
-    //                 },
-    //                 message: 'Spelling matters'
-    //             },
-    //             {
-    //                 location: {
-    //                     uri: textDocument.uri,
-    //                     range: Object.assign({}, diagnostic.range)
-    //                 },
-    //                 message: 'Particularly for names'
-    //             }
-    //         ];
-    //     }
-    //     diagnostics.push(diagnostic);
-    // }
-
-    // // Send the computed diagnostics to VSCode.
-    // connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
 connection.onDidChangeWatchedFiles(_change => {
