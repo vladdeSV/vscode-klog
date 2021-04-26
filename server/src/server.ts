a0667923c742c47fffa39dfabcb1db1450413fd0
Vladimirs Nordholm <v@vladde.net>
2021-04-26 13:44:07 +0200

    comment out more example code (will use as reference later)

--- server/src/server.ts
+++ server/src/server.ts
@@ -137,7 +137,6 @@ documents.onDidChangeContent(change => {
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 
-
     // // In this simple example we get the settings for every validate run.
     // const settings = await getDocumentSettings(textDocument.uri);
 
@@ -203,16 +202,11 @@ connection.onCompletion(
         // which code complete got requested. For the example we ignore this
         // info and always provide the same completion items.
         return [
-            {
-                label: 'TypeScript',
-                kind: CompletionItemKind.Text,
-                data: 1
-            },
-            {
-                label: 'JavaScript',
-                kind: CompletionItemKind.Text,
-                data: 2
-            }
+            // {
+            //     label: 'Item',
+            //     kind: CompletionItemKind.Text,
+            //     data: 1
+            // },
         ];
     }
 );
@@ -221,13 +215,10 @@ connection.onCompletion(
 // the completion list.
 connection.onCompletionResolve(
     (item: CompletionItem): CompletionItem => {
-        if (item.data === 1) {
-            item.detail = 'TypeScript details';
-            item.documentation = 'TypeScript documentation';
-        } else if (item.data === 2) {
-            item.detail = 'JavaScript details';
-            item.documentation = 'JavaScript documentation';
-        }
+        // if (item.data === 1) {
+        //     item.detail = 'Item details';
+        //     item.documentation = 'Item documentation';
+        // }
         return item;
     }
 );
