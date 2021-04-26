cc648288cf335025ec308a5380660a968a4ecdda
Vladimirs Nordholm <v@vladde.net>
2021-04-27 01:13:30 +0200

    (minor)

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -48,7 +48,7 @@ export function activate(context: ExtensionContext) {
 	// Create the language client and start the client.
 	client = new LanguageClient(
 		'klogLanguageServer',
-		'klog Language Server',
+		'klog',
 		serverOptions,
 		clientOptions
 	);
